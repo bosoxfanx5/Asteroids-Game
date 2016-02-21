faec5b20e77dbca63330a5eb0b25018a9605009a
Administrator1 <admin@owners-mbp.lan>
2016-02-20 18:16:27 -0800

    Initial Commit

---
+++ skeet2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  skeet2
+//
+//  Created by Administrator on 2/20/16.
+//  Copyright © 2016 BYU. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+    return 0;
+}
