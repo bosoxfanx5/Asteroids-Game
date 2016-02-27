644e2faae9ffb2a374e829d500ca65f9c69fa8cb
Brooks Robison <brooksrobison@brookss-imac.local>
2016-02-26 19:28:08 -0800

    Edited out comments

--- skeet2/main.cpp
+++ skeet2/main.cpp
@@ -38,7 +38,6 @@ public:
  * engine will wait until the proper amount of
  * time has passed and put the drawing on the screen.
  **************************************/
-//void callBack(const Interface *pUI, void * vp)
 void callBack(const Interface *pUI, void * p)
 {
 /*
@@ -47,10 +46,6 @@ void callBack(const Interface *pUI, void * p)
  |    |  \ \__/  |  |    \__, /--\ |___ |___ |___ |  \
  
 */
-   //Ball * pBall = (Ball *)p;  // cast the void pointer into a known type
-   //std::vector<Ball> ballVector = *(std::vector<Ball> *)vp;
-   //std::vector<Ball> ballVector = *(std::vector<Ball> *)vp;
-   
    
    Frame * pFrame = (Frame *)p;  // cast the void pointer into a known type
    pFrame->banner.draw();
@@ -166,113 +161,6 @@ void callBack(const Interface *pUI, void * p)
    }
    
    pFrame->detectCollisions();
-   
-
-   
-   //Iterate through vector that was passed in...
-//   for (std::vector<Ball>::iterator it  = ballVector.begin();
-//                                    it != ballVector.end();
-//                                  ++it)
-//   {
-//      //Ball * b = &(*it); //This works in place of it.
-//      //Ball * b = &ballVector[vectorOffset];
-//      //Ball * b = &ballVector[vectorOffset];
-//      
-//      
-//      b->pt.addX(offset);
-//      b->pt.addY(offset);
-//      
-//      // move the polygon
-//      //if (pUI->isRight()) pUI->rifle.turnRight();
-//      //if (pUI->isLeft())  pUI->rifle.turnLeft();
-//      
-//      if (pUI->isRight()) b->pt.addX( 1);
-//      if (pUI->isLeft())  b->pt.addX(-1);
-//      if (pUI->isUp())    b->pt.addY( 1);
-//      if (pUI->isDown())  b->pt.addY(-1);
-//   
-//      // rotate constantly
-//      b->rotation++;
-//   
-//      // draw
-//      drawPolygon(b->rotation, //position//
-//                  20,           // radius //
-//                  b->sides,    //segments//
-//                  b->rotation);//rotation//
-//      
-//      ballVector[vectorOffset] = *b;
-//      
-//      offset +=20;
-//      vectorOffset++;
-//   }
-   
-/*
- ___  _ ____ ____ ____ ___
- |  \ | |__/ |___ |     |
- |__/ | |  \ |___ |___  |
-
-*/
-   
-   //Rifle r;
-   //r.draw();
-   
-   //pUI->drawRifle();
-   
-   //Velocity v(10);
-   
-   //Bullet b;
-   //b.setVelocity(v);
-   //b.draw();
-   
-//   for (int i = 0; i < 10; i++)
-//   {
-//      Point bulletLocation = b.getLocation();
-//      bulletLocation.addX(-10);
-//      bulletLocation.addY(10);
-//      b.setLocation(bulletLocation);
-//      b.draw();
-//   }
-   
-   //Pigeon p;
-   //p.draw();
-   
-   //Banner banner;
-   //banner.draw();
-   
-   
-   //drawRect(r.getLocation(), r.width, r.length, r.getOrientation());
-   //drawPolygon(r.pt, 5, 4, r.rotation+=10);
-   
-/*
-   ____ ____ _ ____ _ _  _ ____ _
-   |  | |__/ | | __ | |\ | |__| |
-   |__| |  \ | |__] | | \| |  | |___
-
-*/
-//   // move the polygon
-//   if (pUI->isRight())
-//      pBall->pt.addX(1);
-//   if (pUI->isLeft())
-//      pBall->pt.addX(-1);
-//   if (pUI->isUp())
-//      pBall->pt.addY(1);
-//   if (pUI->isDown())
-//      pBall->pt.addY(-1);
-//   
-//   // use the space bar to change the number of sides.
-//   if (pUI->isSpace())
-//      pBall->sides++;
-//   if (pBall->sides == 12)
-//      pBall->sides = 3;
-//   
-//   // rotate constantly
-//   pBall->rotation++;
-//   
-//   // draw
-//   drawPolygon(pBall->pt, /*position*/
-//               20, /* radius */
-//               pBall->sides /*segments*/,
-//               pBall->rotation /*rotation*/);
 }
 
 /*********************************
